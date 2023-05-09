619417cdb724b298e08572abac5329117c0ceb00
jekser09 <jekser09@gmail.com>
2023-05-09 16:37:35 -0500

    avance index

--- SoporteTecnico/forms.py
+++ SoporteTecnico/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class Login(forms.Form):
     user=forms.CharField(label=False,widget=forms.TextInput(attrs={'placeholder':'Usuario','class':'campos ali'}),max_length=20)
-    password=forms.CharField(label=False,widget=forms.PasswordInput(attrs={'placeholder':'Contraseña','class':'campos ali'}),max_length=20)
-    iniSesion=forms.CharField(widget=forms.TextInput(attrs={'type':'submit','value':'Iniciar Sesion','class':'boton ali','id':'bIni'}))
+    password=forms.CharField(label=False,widget=forms.PasswordInput(attrs={'placeholder':'Contraseña','class':'campos'}),max_length=20)
+    iniSesion=forms.CharField(widget=forms.TextInput(attrs={'type':'submit','value':'Iniciar Sesion','class':'boton','id':'bIni'}))
     registro=forms.CharField(widget=forms.TextInput(attrs={'type':'submit','value':'Registrarse','class':'boton ali','id':'bReg'}))
