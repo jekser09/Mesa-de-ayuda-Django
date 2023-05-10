0bba97a3354f535c8436c24f32deb3b15815127c
jekser09 <jekser09@gmail.com>
2023-05-09 23:18:56 -0500

    avance registro

--- SoporteTecnico/forms.py
+++ SoporteTecnico/forms.py
@@ -3,5 +3,14 @@ from django import forms
 class Login(forms.Form):
     user=forms.CharField(label=False,widget=forms.TextInput(attrs={'placeholder':'Usuario','class':'campos ali'}),max_length=20)
     password=forms.CharField(label=False,widget=forms.PasswordInput(attrs={'placeholder':'Contraseña','class':'campos'}),max_length=20)
-    iniSesion=forms.CharField(widget=forms.TextInput(attrs={'type':'submit','value':'Iniciar Sesion','class':'boton','id':'bIni'}))
-    registro=forms.CharField(widget=forms.TextInput(attrs={'type':'submit','value':'Registrarse','class':'boton ali','id':'bReg'}))
+    iniSesion=forms.CharField(widget=forms.TextInput(attrs={'type':'submit','value':'Ingreso','class':'boton','id':'bIni'}))
+    registro=forms.CharField(widget=forms.TextInput(attrs={'type':'submit','value':'Registro','class':'boton ali','id':'bReg'}))
+
+class Registro(forms.Form):
+    nombres=forms.CharField(label=False,widget=forms.TextInput(attrs={'placeholder':'Nombres'}))
+    apellidos=forms.CharField(label=False,widget=forms.TextInput(attrs={'placeholder':'Apellidos'}))
+    area=forms.ChoiceField(label=False,widget=forms.Select,choices=(('Zona T','Luna'),('Usaquen','Amarti')))
+    id=forms.CharField(label=False,widget=forms.TextInput(attrs={'placeholder':'Cargo'}))
+    numCon=forms.CharField(label=False,widget=forms.TextInput(attrs={'placeholder':'Numero'}))
+    usuario=forms.CharField(label=False,widget=forms.TextInput(attrs={'placeholder':'Usuario','class':'campos'}),max_length=20)
+    password=forms.CharField(label=False,widget=forms.PasswordInput(attrs={'placeholder':'Contraseña','class':'campos'}),max_length=20)
--- SoporteTecnico/views.py
+++ SoporteTecnico/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from .forms import Login
+from .forms import Login,Registro
 # Create your views here.
 def index(request):
-    return render(request,'Index.html',{
+    return render(request,'index.html',{
         'form':Login()
+    })
+
+def registro(request):
+    return render(request,'Registro.html',{
+        'form':Registro()
     })
